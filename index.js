d3faf046375f2185226d8be7dd5d5cd39cc56589
cmhollen <cmhollenkamp@gmail.com>
2018-07-30 21:52:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(arrMusicians, arrInstruments){
+  var arrNew = [];
+  for(var i = 0; i < arrMusicians.length; i++){
+    arrNew.push(`${arrMusicians[i]} plays ${arrInstruments[i]}`);
+  }
+  return arrNew;
+}
